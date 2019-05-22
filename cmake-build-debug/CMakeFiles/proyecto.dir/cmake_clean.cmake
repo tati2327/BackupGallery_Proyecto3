d1fbacf68894c1570beef7c07489600954bfc081
fabian <fabian152195@gmail.com>
2019-05-22 16:06:41 -0600

    test2

---
+++ cmake-build-debug/CMakeFiles/proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyecto.dir/main.cpp.o"
+  "proyecto.pdb"
+  "proyecto"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
