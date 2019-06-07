8470ad350b7e3fc1badad30868110e944954107b
Fabian <fabian152195@gmail.com>
2019-06-07 08:27:41 -0600

    Docummented send Functions
    Head of Class myIDE - docummented
    Functions: UPDATE, SELECT, INSERT, DELETE - docummented

--- MyIDE.h
+++ MyIDE.h
@@ -17,6 +17,12 @@
 #include<stdlib.h>
 
 namespace Ui {
+
+//!Class myIDE: user interacting class
+/*!
+ * This is the class that serves as an interfacte, for the user
+ * be able to communicate with NoSQL database and RAID 5
+ */
 class myIDE;
 }
 
@@ -25,18 +31,58 @@ class myIDE : public QMainWindow
     Q_OBJECT
 
 public:
+    //!Default constructor
     explicit myIDE(QWidget *parent = nullptr);
+
+    //!Default Destructor
     ~myIDE();
+
+    //FALTA COMENTAR
     void putData(int id, QString name, QString author,int year, int size, QString description);
 
+    //!identifyIDEFunction
+    /*!
+     * \param text: text received from user at textBox
+     * Recognizes if the first command typed is either UPDATE, INSERT, SELECT, DELETE.
+     * If not, sends user message to retype an approved command.
+     */
     void identifyIDEFunction(QString text);
 
+    //!sendInsertCommand
+    /*!
+     * \param text: text received from user at textBox
+     * \param textIndex: character position indicator
+     * Validates command and receive name, author, year, size and description
+     * Then forms a JSON string and send it to server
+     */
     void sendInsertCommand(QString text, int textIndex);
 
+    //!sendSelectCommand
+    /*!
+     * \param text: text received from user at textBox
+     * \param textIndex: character position indicator
+     * Validates command and receive name of the column or * (for all columns) desired
+     * Then forms a JSON string and send it to server
+     */
     void sendSelectCommand(QString text, int textIndex);
 
+    //!sendDeleteCommand
+    /*!
+     * \param text: text received from user at textBox
+     * \param textIndex: character position indicator
+     * Validates command and receive column and value that conditions the rows in the
+     * table that will be deleted. Then forms a JSON string and send it to server
+     */
     void sendDeleteCommand(QString text, int textIndex);
 
+    //!sendUpdateCommand
+    /*!
+     * \param text: text received from user at textBox
+     * \param textIndex: character position indicator
+     * Validates command and receive column and value that will be setted
+     * and the column and value that conditions the rows in the table that
+     * will be updated. Then forms a JSON string and send it to server
+     */
     void sendUpdateCommand(QString text, int textIndex);
 
 private slots:
