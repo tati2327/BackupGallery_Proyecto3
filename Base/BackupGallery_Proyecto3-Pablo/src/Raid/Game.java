a228a7d392d33872141f9a50a2edfa1709f20f57
Josue Araya <jdag98228@gmail.com>
2019-06-17 13:56:35 -0600

    Base No sql

---
+++ Base/BackupGallery_Proyecto3-Pablo/src/Raid/Game.java
@@ -0,0 +1,23 @@
+package Raid;
+
+import java.io.IOException;
+
+public class Game {
+	Controller myController;
+	
+	public void saveImage(byte[] incoming, int id) throws IOException {
+		Images img=myController.saveIncomingImage(incoming,id);	
+		myController.loadImage(img);
+		myController.divideImage(null, img);	
+	}
+	
+	/*public byte[] getImage(int id) {
+		
+	}*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
